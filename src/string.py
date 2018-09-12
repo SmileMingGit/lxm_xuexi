9d797063ff48d787973cb3381d857b40c68ebc50
SmileMingGit <651566989@qq.com>
2018-09-12 16:21:20 +0800

    tijiao1

--- src/string.py
+++ src/string.py
@@ -1,7 +1,8 @@
+$ head -2 src/string.py
 #!/usr/bin/python3
 
 var1 = 'Hello World!'
 var2 = "Python Programming"
-
+var3='lkfdlkfldkfld;lfdkfd'
 print ("var1[0]: ", var1[0])
 print ("var2[1:5]: ", var2[1:5]) # 切片加索引
